c1b558b9f50a98b4ded01effe78d8e31394fee6e
Amber Williams <awilliams1234@live.com>
2020-02-17 00:40:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,11 +3,6 @@ def countdown(num)
   puts "#{num} SECOND(S)!"
   num -= 1
 end
-def countdown(num) 
-while num > 0 
-puts "#{num} + "SECOND(S)!" 
-num -= 1 
-end
-'Happy New Year'
+"Happy New Year"
 end
 
